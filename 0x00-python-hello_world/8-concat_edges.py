cb48aaf8250900d62f5bf85f0d089ef907215e5a
Kero Filicha <kerotulu@gmail.com>
2022-09-20 19:15:52 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax"
-         str = str[39:67] + str[107:112] + str[0:6]
-         print(str)
+str = "Python is an interpreted interactive object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+        s = str[5:6] + str[12] + str[0]
+        Print(s)
