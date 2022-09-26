583730817979c601c2c3c2893bb48224f4888ff8
Kero Filicha <kerotulu@gmail.com>
2022-09-26 10:52:19 -0700

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -21,9 +21,8 @@ def replace_in_list(my_list, idx, element):
     """
 
     # Check for negative and out of range index
-    if idx < 0:
+    if idx < 0 or idx > len(my_list) -1:
         return my_list
-    if idx >= len(my_list):
+    else:
+        my_list[idx] = element
         return my_list
-    my_list[idx] = element
-    return my_list
