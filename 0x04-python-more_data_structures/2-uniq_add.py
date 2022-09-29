85b2df1ab615aee92d09eda510604a504d16fcd2
Kero Filicha <kerotulu@gmail.com>
2022-09-29 16:25:10 -0700

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-        return add
+    sum = 0
+    unique_list = []
+    for x in my_list:
+        if x not in unique_list:
+            unique_list.append(x)
+            for x in unique_list:
+                sum += x
+                return (sum)
