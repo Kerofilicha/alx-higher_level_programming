24866be0baa7eb4237aa4b12d6f9e76ba77095cf
Kero Filicha <kerotulu@gmail.com>
2022-10-25 11:04:28 -0700

    Load add save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Defines a JSON file-reading function."""
-import json
+"""Add all arguments to a Python list and save them to a file."""
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
+
+            try:
+                items = load_from_json_file("add_item.json")
+            except FileNotFoundError:
+                items = []
+                items.extend(sys.argv[1:])
+                save_to_json_file(items, "add_item.json")
