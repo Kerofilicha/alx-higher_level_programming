fa03524c890336fec540c8051a5b12f9cf18b5c7
Kero Filicha <kerotulu@gmail.com>
2022-12-21 11:29:59 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < languages.length; ++i) {
-	console.log(languages[i]);
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+	console.log(args[arg]);
 }
