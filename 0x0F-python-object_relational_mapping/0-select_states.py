7a8bcb7bc1e7e6216c3db76b5bf12b9092a0df36
Kero Filicha <kerotulu@gmail.com>
2022-12-27 17:40:20 -0800

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that lists all `states` from the database `hbtn_0e_0_usa`.
 
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-    """
-
+if __name__ == '__main__':
     import sys
     import MySQLdb
 
-    if __name__ == "__main__":
-        mySQL_u = sys.argv[1]
-        mySQL_p = sys.argv[2]
-        db_name = sys.argv[3]
+    if len(sys.argv) != 4:
+        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
+                ' <database name>')
 
-        # By default, it will connect to localhost:3306
-        db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
-        cur = db.cursor()
-
-        cur.execute("SELECT * FROM states ORDER BY id")
-        rows = cur.fetchall()
-
-        for row in rows:
+        conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
             print(row)
+            cur.close()
+            conn.close()
